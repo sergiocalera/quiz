caeea165acdfb947dc511b825583b3ea54813e8c
sergiocalera <sergiocalera@gmail.com>
2015-07-22 13:49:27 -0500

    Despliegue en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -5,7 +5,7 @@ exports.question = function(req, res){
 
 // GET /quizes/answer
 exports.answer = function(req, res){
-	if(req.quey.respuesta === 'Roma'){
+	if(req.query.respuesta.toLowerCase() === 'roma'){
 		res.render('quizes/answer', {respuesta : 'Correcto'});
 	} else{
 		res.render('quizes/answer', {respuesta : 'Incorrecto'})
